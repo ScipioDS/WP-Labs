f370ca5306fb0e800d8da66fec5127e0063d5197
Skipio <davidstojkov23@gmail.com>
2024-11-10 22:50:53 +0100

    having fun atp

--- src/main/java/mk/finki/ukim/mk/lab/web/controller/EventBookingController.java
+++ src/main/java/mk/finki/ukim/mk/lab/web/controller/EventBookingController.java
@@ -1,10 +1,13 @@
 package mk.finki.ukim.mk.lab.web.controller;
 
+import mk.finki.ukim.mk.lab.model.EventBooking;
 import mk.finki.ukim.mk.lab.service.EventBookingService;
 import mk.finki.ukim.mk.lab.service.EventService;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,15 +26,18 @@ public class EventBookingController {
                             @RequestParam("chosen_event") String selectedEvent, // Extract selected radio button
                             @RequestParam("numTickets") int numTickets, // Extract number of tickets
                             @RequestParam("bookbool") String bookbool, // Confirm booking action,
-                            Model model
+                            Model model,
+                            @RequestHeader("User-Agent") String userAgent,
+                            @RequestHeader("Origin") String origin
     ){
         if (error != null && !error.isEmpty()) {
             model.addAttribute("hasError", true);
             model.addAttribute("error", error);
         }
 
-        eventBookingService.placeBooking(selectedEvent, "David", "K1", numTickets);
-        model.addAttribute("booking", eventBookingService.getBooking());
+        // eventBookingService.placeBooking(selectedEvent, "David", "K1", numTickets);
+        EventBooking eventBooking = new EventBooking(selectedEvent, userAgent, origin, (long) numTickets);
+        model.addAttribute("booking", eventBooking);
         return "bookingConfirmation";
     }
 }
